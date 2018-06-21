f988ae9e7fa4ee539304310172f8caf7a5014b81
Snyp code <snypcode@email.com>
2018-06-21 10:38:11 +0300

    -key replace --key

--- start.py
+++ start.py
@@ -8,11 +8,11 @@ file = File()
 if __name__ == "__main__":
     if len(sys.argv) > 1 and len(sys.argv) < 5:
         # комбинации ключей в работе с файлом
-        if sys.argv[1] == '-eng' and ('-path' in sys.argv) and len(sys.argv) == 4:
+        if sys.argv[1] == '--eng' and ('--path' in sys.argv) and len(sys.argv) == 4:
             if file.path_exists(sys.argv[-1]): exit(1)
             if layout.read_file(file.open_file()): print('данные не верны'); exit(1)
             result = layout.eng_to_rus()
-        elif sys.argv[1] == '-rus' and ('-path' in sys.argv) and len(sys.argv) == 4:
+        elif sys.argv[1] == '--rus' and ('--path' in sys.argv) and len(sys.argv) == 4:
             if file.path_exists(sys.argv[-1]): exit(1)
             if layout.read_file(file.open_file()): print('данные не верны'); exit(1)
             result = layout.rus_to_eng()
@@ -23,11 +23,11 @@ if __name__ == "__main__":
 
         # комбинации ключей без файла
         layout.get_text(sys.argv[-1])
-        if sys.argv[1] == '-eng' and len(sys.argv) == 3:
+        if sys.argv[1] == '--eng' and len(sys.argv) == 3:
             result = layout.eng_to_rus()
-        elif sys.argv[1] == '-rus' and len(sys.argv) == 3:
+        elif sys.argv[1] == '--rus' and len(sys.argv) == 3:
             result = layout.rus_to_eng()
-        elif len(sys.argv) == 2 and (not '-rus' in sys.argv ) and (not '-eng' in sys.argv) and (not '-path' in sys.argv):
+        elif len(sys.argv) == 2 and (not '--rus' in sys.argv ) and (not '--eng' in sys.argv) and (not '--path' in sys.argv):
             result = layout.auto_translate()
         else:
             print('неверный порядок параметров')
